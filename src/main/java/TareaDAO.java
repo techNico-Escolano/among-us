d754e639deff8dd510fbce4fbf22e713dc9f2b49
Nicolas Escolano <nicoescolano@icloud.com>
2026-03-12 09:59:34 +0100

    Clases Tarea, TareaDAO, TareaDAOImpl

--- src/main/java/TareaDAO.java
+++ src/main/java/TareaDAO.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public interface TareaDAO {
+    void insertar(Tarea tarea);
+    Tarea obtener(int id);
+    ArrayList<Tarea> obtenerTodos();
+    ArrayList<Tarea> obtenerPorTripulante(int idTrip);
+    void actualizar(Tarea tarea);
+    void eliminar(int id);
 }
--- src/main/java/TareaDAOImpl.java
+++ src/main/java/TareaDAOImpl.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class TareaDAOImpl {
+public class TareaDAOImpl implements TareaDAO{
+
+    @Override
+    public void insertar(Tarea tarea) {
+
+    }
+
+    @Override
+    public Tarea obtener(int id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Tarea> obtenerTodos() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Tarea> obtenerPorTripulante(int idTrip) {
+        return null;
+    }
+
+    @Override
+    public void actualizar(Tarea tarea) {
+
+    }
+
+    @Override
+    public void eliminar(int id) {
+
+    }
 }
